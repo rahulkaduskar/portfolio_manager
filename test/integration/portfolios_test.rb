12de069701f92de4967cd1389d654fcf69a8e882
rahulkaduskar <rahulkaduskar@gmail.com>
2016-05-09 13:31:03 +0530

    added more integration test cases...

--- test/integration/portfolios_test.rb
+++ test/integration/portfolios_test.rb
@@ -25,7 +25,6 @@ class PortfoliosTest < ActionDispatch::IntegrationTest
     visit root_path   
     click_link 'Upload Portfolio'
     assert page.has_content?('Upload your Portfolios:')
-    click_link 'Upload Portfolio'
     script = "$('#fileupload').css({opacity: '1'});"
     page.execute_script(script)
     attach_file(:file, "#{Rails.root}/test/fixtures/files/portfolio.csv")
@@ -33,4 +32,41 @@ class PortfoliosTest < ActionDispatch::IntegrationTest
     assert page.has_content?('AAPL')
   end
 
+  test "Check valid invalid current share price file upload " do
+    visit root_path   
+    click_link 'Upload Current Price'
+    assert page.has_content?('Upload current Share Price:')
+    script = "$('#fileupload').css({opacity: '1'});"
+    page.execute_script(script)
+    attach_file(:file, "#{Rails.root}/test/fixtures/files/invalidfile.csv")
+    assert page.has_content?('Line format issue in csv file: Please check line => AAPL,,23.24')
+  end
+
+  test "Check valid current share price file upload " do
+    visit root_path   
+    click_link 'Upload Current Price'
+    assert page.has_content?('Upload current Share Price:')
+    script = "$('#fileupload').css({opacity: '1'});"
+    page.execute_script(script)
+    attach_file(:file, "#{Rails.root}/test/fixtures/files/sharePrice.csv")
+    assert page.has_content?('Your Portfolios')
+    assert page.has_content?('Portfolios not available.')
+  end
+
+  test "Check valid portfolio with current share price file and portfolio file " do
+    visit root_path   
+    click_link 'Upload Portfolio'
+    assert page.has_content?('Upload your Portfolios:')
+    script = "$('#fileupload').css({opacity: '1'});"
+    page.execute_script(script)
+    attach_file(:file, "#{Rails.root}/test/fixtures/files/portfolio.csv")
+    click_link 'Upload Current Price'
+    assert page.has_content?('Upload current Share Price:')
+    script = "$('#fileupload').css({opacity: '1'});"
+    page.execute_script(script)
+    attach_file(:file, "#{Rails.root}/test/fixtures/files/sharePrice.csv")
+    assert page.has_content?('Your Portfolios')
+    assert page.has_content?('AAPL')
+    assert page.has_content?('-70.0')
+  end
 end
